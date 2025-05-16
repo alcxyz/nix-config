fff3fe28f271f7b3e35268774ab98d59884b8130
alcxyz <me@alc.no>
2025-05-16 23:44:45 +0200

    add desktop from polarity for import

---
+++ desktop/addons/swaync/default.nix
@@ -0,0 +1,22 @@
+{ options
+, config
+, lib
+, pkgs
+, ...
+}:
+with lib;
+with lib.custom; let
+  cfg = config.desktop.addons.swaync;
+in
+{
+  options.desktop.addons.swaync = with types; {
+    enable = mkBoolOpt false "Enable or disable swaync";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      swaynotificationcenter
+      libnotify
+    ];
+  };
+}
