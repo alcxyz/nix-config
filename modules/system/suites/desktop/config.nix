3bb08c7349ed1149aea7ca431cfa18575c361542
post <post@alc.no>
2025-05-17 00:48:48 +0000

    fix nix-colors and wallpapers

--- modules/system/suites/desktop/config.nix
+++ modules/system/suites/desktop/config.nix
@@ -7,6 +7,12 @@ with lib;
       default = false;
       description = "Enable the desktop system configuration (services, features)."; # Updated description
     };
+    # Add the autoLogin option for the system level
+    autoLogin = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable automatic login for the desktop user.";
+    };
   };
 
   config = mkIf config.desktop.enable {
@@ -26,6 +32,12 @@ with lib;
       enable = true;
       displayManager.gdm.enable = true;
       desktopManager.gnome.enable = true;
+
+      # Add autoLogin configuration
+      displayManager.autoLogin = mkIf config.desktop.autoLogin {
+        enable = true;
+        user = config.users.users.${config.username}.name; # Use config.users.users.${config.username}.name to get the primary user's name
+      };
     };
 
     # Removed deluge and calibre-web service configurations
