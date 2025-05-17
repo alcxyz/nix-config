5afdaa64dd85d4276c9d8211014200ea07abbe26
alcxyz <me@alc.no>
2025-05-17 17:53:41 +0200

    clean, host specific and generic

--- modules/home/environment.nix
+++ modules/home/environment.nix
@@ -5,7 +5,7 @@
     home.sessionVariables = {
       EDITOR = "nvim";
       DIRENV_LOG_FORMAT = ""; # Moved from users/alc/home.nix
-      FLAKE = "/home/alc/polarity"; # Added from old user module
+      FLAKE = "/home/alc/nix-config"; # Added from old user module
     };
   };
 }
