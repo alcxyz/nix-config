596c2bb3a8e35f1eecbe3d6f38b7053217f24b26
alcxyz <me@alc.no>
2025-06-09 03:08:57 +0200

    add alc_xyz_key to authorized_keys

--- modules/nixos/services/ssh/default.nix
+++ modules/nixos/services/ssh/default.nix
@@ -11,6 +11,7 @@ with lib;
 
 let
   cfg = config.services.openssh;
+  alc_xyz_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM9g7HJbiqvmCZRZF5z5g9J/VLI91p7RpXipA9eWHX2q alc@xyz";
   alc_mac_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAxWjN37TvOrWjv1FXde72TscMwP0TbHRhoe0kO8IIU0 alc@AM-VYH2F56CR6";
   alc_iphone_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEhgqS6A8n44Azg65g9u7a2mQ+RwqYo8dBW/4CHfua+0";
 in
@@ -29,6 +30,7 @@ in
     users.users = {
       root.openssh.authorizedKeys.keys = [ alc_mac_key ];
       ${username}.openssh.authorizedKeys.keys = [
+        alc_xyz_key
         alc_mac_key
         alc_iphone_key
       ];
