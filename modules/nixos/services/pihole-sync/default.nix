d45dd4e1887538d3346d81efc6573b379381e0d9
alcxyz <me@alc.no>
2025-11-09 17:20:16 +0100

    fix

--- modules/nixos/services/pihole-sync/default.nix
+++ modules/nixos/services/pihole-sync/default.nix
@@ -2,7 +2,7 @@
 
 {
   systemd.services.pihole-sync = {
-    description = "Pi-hole Teleporter Sync (NUC → RPi)";
+    description = "Pi-hole Teleporter Sync (NUX → RPi)";
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
 
@@ -10,7 +10,9 @@
       Type = "oneshot";
       User = "root";
       Group = "root";
-      ExecStart = "${pkgs.bash}/bin/bash -c 'export PIHOLE_ADMIN_PASSWORD=$(cat ${config.sops.secrets.pihole_secret_key.path}); /home/alc/dev/alcxyz/gitops/nux/pihole/pihole-sync'";
+      StandardOutput = "journal";
+      StandardError = "journal";
+      ExecStart = "${pkgs.bash}/bin/bash -c 'export PIHOLE_ADMIN_PASSWORD=$(cat ${config.sops.secrets.pihole_secret_key.path}); /home/alc/dev/git/alcxyz/gitops/nux/pihole/scripts/pihole-sync/pihole-sync -config /home/alc/dev/git/alcxyz/gitops/nux/pihole/scripts/pihole-sync/config.toml'";
     };
   };
 
@@ -18,7 +20,7 @@
     description = "Hourly Pi-hole sync timer";
     wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnBootSec = "5min";
+      OnBootSec = "10min";
       OnUnitActiveSec = "1h";
       Persistent = true;
     };
