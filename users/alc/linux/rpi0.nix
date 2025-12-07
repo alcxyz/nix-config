e81db53f90504f09f7959c3dc0784072aac3d8af
alcxyz <me@alc.no>
2025-12-07 12:29:18 +0100

    add rpi0

---
+++ users/alc/linux/rpi0.nix
@@ -0,0 +1,20 @@
+# users/alc/linux/rpi0.nix
+{ inputs, configDir, pkgs, ... }:
+
+let
+  pkgsets = import "${configDir}/modules/nixos/common/pkgsets.nix" {
+    inherit pkgs inputs;
+  };
+in
+
+{
+  # Import the common Linux configuration.
+  imports = [ "${configDir}/users/alc/common.nix" ];
+
+  home.packages = pkgsets.home.server;
+  # That's it!
+  # If you ever need to add a package or setting *only* for nux,
+  # you would add it here. For example:
+  #
+  # home.packages = [ pkgs.tmux ];
+}
