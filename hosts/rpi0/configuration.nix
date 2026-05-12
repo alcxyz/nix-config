680701aaa96755e8c72ebcf35a37d99e61e68fee
alcxyz <6753563+alcxyz@users.noreply.github.com>
2026-05-12 02:44:15 +0200

    Add k8s API VIP

--- hosts/rpi0/configuration.nix
+++ hosts/rpi0/configuration.nix
@@ -14,6 +14,7 @@
     "${configDir}/modules/nixos/common/server.nix"
     "${configDir}/modules/nixos/services/pihole-native/default.nix"
     "${configDir}/modules/nixos/services/unifi-native/default.nix"
+    "${configDir}/modules/nixos/services/k8s-api-vip/default.nix"
     "${configDir}/modules/nixos/virtualisation/k3s/default.nix"
     "${configDir}/modules/nixos/services/netbird/default.nix"
   ];
@@ -104,7 +105,24 @@
 
   k3s = {
     enable = true;
-    serverAddr = "https://192.168.1.15:6443";
+    serverAddr = "https://k8s-api.local:6443";
     tokenFile = config.sops.secrets.k3s_server_token.path;
+    tlsSans = [
+      "k8s-api.local"
+      "192.168.1.250"
+    ];
   };
+
+  services.k8s-api-vip = {
+    enable = true;
+    interface = "end0";
+    sourceIp = "192.168.1.3";
+    peers = [
+      "192.168.1.15"
+      "192.168.1.16"
+    ];
+    priority = 90;
+  };
+
+  networking.hosts."192.168.1.250" = ["k8s-api.local"];
 }
---
+++ modules/nixos/services/k8s-api-vip/default.nix
@@ -0,0 +1,94 @@
+# modules/nixos/services/k8s-api-vip/default.nix
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.services.k8s-api-vip;
+in {
+  options.services.k8s-api-vip = {
+    enable = mkEnableOption "a floating LAN VIP for the k3s API server";
+
+    interface = mkOption {
+      type = types.str;
+      description = "LAN interface that should carry the Kubernetes API VIP.";
+    };
+
+    sourceIp = mkOption {
+      type = types.str;
+      description = "This node's LAN IP address used for unicast VRRP.";
+    };
+
+    peers = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      description = "Other keepalived peer LAN IPs for unicast VRRP.";
+    };
+
+    vip = mkOption {
+      type = types.str;
+      default = "192.168.1.250";
+      description = "Floating LAN IP address for Kubernetes API clients.";
+    };
+
+    prefixLength = mkOption {
+      type = types.ints.between 1 32;
+      default = 24;
+      description = "CIDR prefix length for the floating VIP.";
+    };
+
+    virtualRouterId = mkOption {
+      type = types.ints.between 1 255;
+      default = 43;
+      description = "VRRP router ID for the Kubernetes API VIP.";
+    };
+
+    priority = mkOption {
+      type = types.ints.between 1 255;
+      default = 100;
+      description = "VRRP election priority for this node.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.keepalived = {
+      enable = true;
+      openFirewall = true;
+      enableScriptSecurity = true;
+
+      vrrpScripts.k3s_api = {
+        script = "${pkgs.netcat-openbsd}/bin/nc -z -w 1 127.0.0.1 6443";
+        interval = 2;
+        timeout = 1;
+        fall = 2;
+        rise = 2;
+        weight = -80;
+        user = "root";
+      };
+
+      vrrpInstances.k8s_api = {
+        interface = cfg.interface;
+        state = "BACKUP";
+        virtualRouterId = cfg.virtualRouterId;
+        priority = cfg.priority;
+        noPreempt = true;
+        unicastSrcIp = cfg.sourceIp;
+        unicastPeers = cfg.peers;
+        virtualIps = [
+          {
+            addr = "${cfg.vip}/${toString cfg.prefixLength}";
+            dev = cfg.interface;
+          }
+        ];
+        trackScripts = ["k3s_api"];
+        extraConfig = ''
+          advert_int 1
+          garp_master_delay 1
+          garp_master_repeat 3
+        '';
+      };
+    };
+  };
+}
