e05025f7e1d35a5fee13ad5c7aca5dc6cccfa36e
alcxyz <me@alc.no>
2026-06-07 16:53:30 +0200

    Deploy paperweight config through home-manager

--- users/alc/linux/operator.nix
+++ users/alc/linux/operator.nix
@@ -1,5 +1,15 @@
-{inputs, ...}: {
+{
+  config,
+  configDir,
+  inputs,
+  ...
+}:
+{
   imports = [
     inputs.nix-secrets.homeManagerModules.linuxOperator
   ];
+
+  xdg.configFile."paperweight/config.toml".text = ''
+    accounts_toml_path = "${config.programs.workspace.root}/platform/regnskap/cmd/bokfor/accounts.toml"
+  '';
 }
