59251d4ffc5abe91726655b2a41e662548a7cd1a
alcxyz <me@alc.no>
2026-07-12 00:20:13 +0200

    fix(metallb): allow speaker memberlist traffic

--- modules/nixos/virtualisation/k3s/default.nix
+++ modules/nixos/virtualisation/k3s/default.nix
@@ -105,10 +105,12 @@ in {
       6443 # K3s API Server
       2379 # K3s etcd client port
       2380 # K3s etcd peer port
+      7946 # MetalLB speaker memberlist
       10250 # Kubelet metrics endpoint for Metrics Server
     ];
     networking.firewall.allowedUDPPorts = [
       8472 # Flannel VXLAN backend
+      7946 # MetalLB speaker memberlist
     ];
 
     # Pod and service traffic traverses the node over the CNI bridge and
