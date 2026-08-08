8c2d15e61a4e5528b6381cf55e48cba28926f401
alcxyz <me@alc.no>
2026-08-08 22:30:37 +0200

    fix(wolf): arbitrate cooperative pointer input

--- modules/nixos/services/wolf-streaming/default.nix
+++ modules/nixos/services/wolf-streaming/default.nix
@@ -19,7 +19,7 @@
   protectedStateDirectory = cfg.protectedProfile.stateDirectory;
   protectedPort = standard: standard + cfg.protectedProfile.portOffset;
   wolfRevision = "d6d41dec9cf758b086768e19a7dc02c20ffce22c";
-  wolfPatchSet = "altgr-idle-presentation-interpipe-media-v8";
+  wolfPatchSet = "altgr-idle-presentation-interpipe-media-pointer-v9";
   wolfBaseImage = "ghcr.io/games-on-whales/wolf@sha256:8515dd1a88fa6c4a39a814c7c2f7eee4106d5b60c8140be6d0ef689324a079a2";
   wolfPatchedImage = "nixbox/wolf:${builtins.substring 0 12 wolfRevision}-${wolfPatchSet}";
   wolfSource = pkgs.fetchFromGitHub {
@@ -38,6 +38,7 @@
       ./wolf-image/reset-interpipe-on-producer-switch.patch
       ./wolf-image/app-producer-buffer-caps.patch
       ./wolf-image/media-keys.patch
+      ./wolf-image/cooperative-pointer-lease.patch
     ];
   };
   wolfBuildContext =
