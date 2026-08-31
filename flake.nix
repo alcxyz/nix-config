263b340718707348fa06601f57311e1f412e58b0
alcxyz <me@alc.no>
2026-08-31 17:57:51 +0200

    chore(storage): repin migration to private dev

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 
     nix-secrets = {
       #url = "path:/home/alc/nix-secrets";
-      url = "git+https://git.alc.xyz/alcxyz/nix-secrets.git?ref=agent/tank-bulk-secure-split";
+      url = "git+https://git.alc.xyz/alcxyz/nix-secrets.git?ref=dev";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
